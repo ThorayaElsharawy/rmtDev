80e7ca7f8ff0e38b4faf3bde6d7518e9d9f5d638
thoraya elsharawy <thorayaelsharawy@gmail.com>
2024-07-16 12:23:41 +0300

    strucure app

---
+++ src/components/BookmarksButton.tsx
@@ -0,0 +1,12 @@
+import { FaRegBookmark } from "react-icons/fa";
+
+
+export default function BookmarksButton() {
+  return (
+    <section>
+      <button className="bookmarks-btn">
+        Bookmarks <FaRegBookmark />
+      </button>
+    </section>
+  );
+}
